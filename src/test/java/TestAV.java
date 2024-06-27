116cd2ff25afad2feaba58d1eeb07369a0f567af
Basil Akavity <vakavity@gmail.com>
2024-06-27 14:26:59 +0300

    Initial commit

---
+++ src/test/java/TestAV.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.SelenideElement;
+import org.example.utils.Utils;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+
+public class TestAV extends BaseTest {
+
+    @Test
+    public void exampleTest() {
+        SelenideElement allowButton = $(By.id("com.android.permissioncontroller:id/permission_allow_button"));
+
+        Utils.waitUntilClickable(allowButton);
+        allowButton.click();
+
+        SelenideElement clickView = $(By.id("clickView"));
+        clickView.click();
+
+        SelenideElement myAdText = $(By.xpath("//android.widget.TextView[@text='Мои объявления']"));
+
+        Assert.assertTrue(myAdText.isDisplayed());
+    }
+}
