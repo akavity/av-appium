c59bcc96e74072c95bb42951786fe0ffa4e66630
Basil Akavity <vakavity@gmail.com>
2024-06-27 19:31:09 +0300

    used lombok Getter

--- src/main/java/org/example/pages/AvPage.java
+++ src/main/java/org/example/pages/AvPage.java
@@ -1,25 +1,14 @@
 package org.example.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import lombok.Getter;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
 
+@Getter
 public class AvPage {
     private final SelenideElement allowButton = $(By.id("com.android.permissioncontroller:id/permission_allow_button"));
     private final SelenideElement viewButton = $(By.id("clickView"));
     private final SelenideElement myAdText = $(By.xpath("//android.widget.TextView[@text='Мои объявления']"));
-
-    public SelenideElement getAllowButton() {
-        return allowButton;
-    }
-
-    public SelenideElement getViewButton() {
-
-        return viewButton;
-    }
-
-    public SelenideElement getMyAdText() {
-        return myAdText;
-    }
 }
